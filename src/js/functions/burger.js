15215cd611281676a67914845aa8f0c224145fcd
HubCrag <hubcrag@gmail.com>
2022-03-14 11:10:17 +0300

    initial

--- src/js/functions/burger.js
+++ src/js/functions/burger.js
@@ -1,42 +1,31 @@
-import { disableScroll } from '../functions/disable-scroll';
-import { enableScroll } from '../functions/enable-scroll';
+const burger = document.querySelector('.burger')
+const menu = document.querySelector('nav')
 
-(function(){
-  const burger = document?.querySelector('[data-burger]');
-  const menu = document?.querySelector('[data-menu]');
-  const menuItems = document?.querySelectorAll('[data-menu-item]');
-  const overlay = document?.querySelector('[data-menu-overlay]');
+const disScroll = () => {
+    let pagePosition = window.scrollY
+    document.body.classList.add('dis--scroll')
+    document.body.dataset.position = pagePosition
+    document.body.style.top = - pagePosition + "px"
+}
 
-  burger?.addEventListener('click', (e) => {
-    burger?.classList.toggle('burger--active');
-    menu?.classList.toggle('menu--active');
+const enScroll = () => {
+    let pagePosition =  parseInt(document.body.dataset.position, 10)
+    document.body.style.top = 'auto'
+    document.body.classList.remove('dis--scroll')
+    window.scrollTo({
+        top: pagePosition,
+        left: 0
+    })
+    document.body.removeAttribute('data-position')
+}
 
-    if (menu?.classList.contains('menu--active')) {
-      burger?.setAttribute('aria-expanded', 'true');
-      burger?.setAttribute('aria-label', 'Close');
-      disableScroll();
+burger.addEventListener('click', (e) => {
+    burger.classList.toggle('is-active')
+    menu.classList.toggle('is-active')
+    if(burger.classList.contains('is-active')) {
+        disScroll()
     } else {
-      burger?.setAttribute('aria-expanded', 'false');
-      burger?.setAttribute('aria-label', 'Open');
-      enableScroll();
+        enScroll()
     }
-  });
+})
 
-  overlay?.addEventListener('click', () => {
-    burger?.setAttribute('aria-expanded', 'false');
-    burger?.setAttribute('aria-label', 'Open');
-    burger.classList.remove('burger--active');
-    menu.classList.remove('menu--active');
-    enableScroll();
-  });
-
-  menuItems?.forEach(el => {
-    el.addEventListener('click', () => {
-      burger?.setAttribute('aria-expanded', 'false');
-      burger?.setAttribute('aria-label', 'Open');
-      burger.classList.remove('burger--active');
-      menu.classList.remove('menu--active');
-      enableScroll();
-    });
-  });
-})();
